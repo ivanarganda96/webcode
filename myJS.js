d1d9c78483028e118d07cca66095b08a728cccd6
ivanarganda96 <76394300+ivanarganda96@users.noreply.github.com>
2022-03-14 18:58:45 +0100

    Update and rename myJS to myJS.js

---
+++ myJS.js
@@ -0,0 +1,11 @@
+/*The Snail in the Well
+
+
+The snail climbs up 7 feet each day and slips back 2 feet each night.
+How many days will it take the snail to get out of a well with the given depth?
+
+Sample Input:
+31
+
+Sample Output:
+6*/
